c842212f237f7fcbb5db05af2a2469ff67f68a19
Madhumathy Fernando <madhufdo@gmail.com>
2018-07-28 08:54:41 +1000

    fix error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import {header} from './components/common';
+import { Header } from './components/common/Header';
 
 class App extends Component {
   render() {
     return (
-    <Header headerText="Authentication" />
-    <View>
-      <Text> An App </Text>
-    </View>
+      <View>
+        <Header />
+        <Text> An App </Text>
+      </View>
     );
   }
 }
